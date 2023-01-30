3904d7ce2ab0cfceb0cdb58f1058ac07cc8addc4
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-01-30 21:11:27 +0900

    [level 1] Title: 폰켓몬, Time: 0.38 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,12 @@
+def solution(nums):
+    answer = 0
+    numsLen = len(nums)
+    newNums = set(nums)
+    if(len(newNums) < numsLen):
+        if(len(newNums) >= (numsLen // 2)):
+            answer = numsLen // 2
+        else:
+            answer = len(newNums)
+    else:
+        answer = numsLen // 2
+    return answer
