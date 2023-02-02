264e74a3198ee451e9aac8958eb46a954a72b435
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-02 21:27:54 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 428 ms, Memory: 56104 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,31 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split(" "))
+
+pDic = {} # 포켓몬의 도감을 딕셔너리 형태로 구현
+
+for i in range(1, N + 1):
+    S = sys.stdin.readline().rstrip()
+    if(ord(S[len(S) - 1]) - 32 != ord(S[len(S) - 1].upper())): # 맨 뒷자리가 대문자인경우! (아스키코드로 확인 대문자 소문자 차이 32)
+        num = str(i) +","
+        pDic[S] = num # 맨뒷자리가 대문자일때 콤마 표시를 통해 확인
+    else:
+        pDic[S] = i
+
+pDicRev = dict(map(reversed, pDic.items())) # 포켓몬 도감 딕셔너리 키 밸류 값 반대인 사전 만들기
+
+answer = []
+
+for i in range(M):
+    S = sys.stdin.readline().rstrip()
+    if(S.isdigit()): # S가 숫자인경우 pDic 사용
+        try:
+            print(pDicRev[S + ","])
+        except:
+            print(pDicRev[int(S)])
+    else: # S가 문자인경우 pDicRev 사용
+        num = pDic[S]  
+        try:
+            print(int(num.rstrip(",")))
+        except:
+            print(int(num)) 
