73505a29b1b4af539df45125e83f0f504ceac810
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-05 19:12:32 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,21 @@
 import sys
 
-N, M = map(int, sys.stdin.readline().split())
+N, M = map(int, sys.stdin.readline().split(" "))
 
-arr = []
+arr = [False for i in range(0, N + 1)] # 탐색여부 배열 생성
 
-flag = [False] * (N + 1)
+answers = [] # 정답 배열 생성
 
-def recue(deepNum):
-    if(deepNum == M):
-        print(" ".join(map(str, arr)))
-        return
+def back(arr, depth):
+    if(depth == M): # 만약 깊이가 원하는 목표인 M까지 갔을경우 출력
+        print(" ".join(map(str, answers)))
+        return  # 이후 다시 되돌아가기
     for i in range(1, N + 1):
-        if(not flag[i]):
-            flag[i] = True # 방문
-            arr.append(i)
-            recue(deepNum + 1)
-            flag[i] = False
-            arr.pop()
+        if(arr[i] != True):
+            answers.append(i)
+            arr[i] = True
+            back(arr, depth + 1) # 깊이 1 증가 
+            answers.pop() # 여기서는 이미 출력을 하나 했기에 하나를 빼기
+            arr[i] = False # 뺀 값은 방문값을 False로 다시지정
 
-recue(0)
+back(arr, 0)
