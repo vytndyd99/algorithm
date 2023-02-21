489a33c6f8490835ba905f889fd644df5154f3da
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-21 20:04:46 +0900

    [Silver II] Title: 이항 계수 2, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,9 +1,8 @@
 import sys
 import math
 
-def com(n, k):
-    return math.factorial(n) // (math.factorial(n - k) * math.factorial(k))
-
 N, K = map(int, sys.stdin.readline().split(" "))
 
-print(com(N, K) % 10007)
+num = math.factorial(N) // (math.factorial(K) * math.factorial(N - K)) % 10007
+
+print(num)
