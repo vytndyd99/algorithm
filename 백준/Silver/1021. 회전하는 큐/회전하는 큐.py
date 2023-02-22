1682f0d8d4982fde4283e17772661b4082e3ebc1
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-22 19:34:59 +0900

    [Silver III] Title: 회전하는 큐, Time: 64 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -3,25 +3,25 @@ import collections
 
 N, M = map(int, sys.stdin.readline().split(" "))
 
-indexArr = list(map(int, sys.stdin.readline().split(" ")))
+q = collections.deque([])
 
-Arr = collections.deque([i for i in range(1, N + 1)]) # 1 ~ N까지의 배열을 덱으로 지정
+for i in range(1, N + 1):
+    q.append(i)
 
-count = 0 # 2, 3 함수 실행 카운트
+idx = list(map(int, sys.stdin.readline().split(" ")))
 
-for s in indexArr: # 찾아야 하는 값들을 모두 확인
-    while(True):
-        if(s == Arr[0]): 
-            Arr.popleft()
-            break
+count = 0
+
+for s in idx:
+    while(q.index(s) != 0):
+        if(q.index(s) < len(q) // 2 + 1):
+            num = q.popleft()
+            q.append(num)
+            count += 1
         else:
-            if(Arr.index(s) <= (len(Arr) // 2)):
-                num = Arr.popleft()
-                Arr.append(num)
-                count += 1
-            else:
-                num = Arr.pop()
-                Arr.appendleft(num)
-                count += 1
+            num = q.pop()
+            q.appendleft(num)
+            count += 1
+    q.popleft()
 
 print(count)
