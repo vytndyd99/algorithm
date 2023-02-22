cf94d97e181be3596cda25e2ee2f4c829e3bdd8a
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-22 17:53:50 +0900

    [Silver I] Title: 회의실 배정, Time: 236 ms, Memory: 52036 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -5,20 +5,17 @@ N = int(sys.stdin.readline())
 times = []
 
 for i in range(N):
-    startTime, endTime = map(int, sys.stdin.readline().split(" "))
-    times.append((startTime, endTime))
+    start, end = map(int, sys.stdin.readline().split(" "))
+    times.append((start, end))
 
 times.sort(key = lambda x : (x[1], x[0]))
 
-count = 1 # 회의 최대 개수
+start = 0
+count = 0
 
-startTime = times[0][0]
-endTime = times[0][1]
-
-for i in range(1, N):
-    if(times[i][0] >= endTime):
-        startTime = times[i][0]
-        endTime = times[i][1]
+for s in times:
+    if(start <= s[0]):
         count += 1
+        start = s[1]
 
 print(count)
