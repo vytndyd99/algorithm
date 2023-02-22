1a208922c0be0e19223433553258d319be01d570
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-23 00:01:49 +0900

    [Silver I] Title: 절댓값 힙, Time: 144 ms, Memory: 39960 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,23 +1,22 @@
 import sys
 import heapq
 
-def absHeap(heap, num): # 절댓값 힙 함수 정의
-    if(num < 0): # 원소의 값이 음수일경우
-        otherNum = -(num)
-        heapq.heappush(heap,(otherNum, num))
-    else: # 0 혹은 양수일경우
-        heapq.heappush(heap, (num, num)) 
+heap = []
+
+def absHeap(heap, num):
+    if(num >= 0):
+        heapq.heappush(heap, (num, num))
+    else:
+        heapq.heappush(heap, (-num, num))
 
 N = int(sys.stdin.readline())
 
-h = [] # 힙 배열 생성
-
 for i in range(N):
-    x = int(sys.stdin.readline())
-    if(x == 0): # x가 0일때
-        if(len(h) == 0): # 힙의 크기가 0이면
+    func = int(sys.stdin.readline())
+    if(func == 0):
+        if(len(heap) == 0):
             print(0)
         else:
-            print(heapq.heappop(h)[1])
+            print(heapq.heappop(heap)[1])
     else:
-        absHeap(h, x)
+        absHeap(heap, func)
