88b1466029065e1617a240b94ddcf44cd4a670fd
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-22 18:45:08 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 50936 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,18 +1,16 @@
 import sys
 import collections
 
-
 N = int(sys.stdin.readline())
 
-deq = collections.deque()
+q = collections.deque([])
 
 for i in range(1, N + 1):
-    deq.append(i)
+    q.append(i)
 
-while(len(deq) > 1): # 덱의 크기가 1보다 클때동안 반복
-    deq.popleft()
-    num = deq.popleft()
-    deq.append(num)
+while(len(q) > 1):
+    q.popleft()
+    num = q.popleft()
+    q.append(num)
 
-# 모든 과정을 거치면 한개가 남으니 그 값이 정답
-print(deq[0])
+print(q[0])
