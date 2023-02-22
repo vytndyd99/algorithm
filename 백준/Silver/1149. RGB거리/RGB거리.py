8b6133ce2681104eab2a9f84b57cae817874d1f2
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-22 16:06:00 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -2,17 +2,18 @@ import sys
 
 N = int(sys.stdin.readline())
 
-sumPrices = [[0 for i in range(N)] for i in range(N)] # 가격의 합계를 나타내는 배열
+prices = []
 
 for i in range(N):
-    colors = list(map(int, sys.stdin.readline().split(" ")))
-    sumPrices[i][0] = colors[0]
-    sumPrices[i][1] = colors[1]
-    sumPrices[i][2] = colors[2]
+    red, green, blue = map(int, sys.stdin.readline().split(" "))
+    prices.append((red, green, blue))
+
+sumPrices = [prices[0]]
 
 for i in range(1, N):
-    sumPrices[i][0] = sumPrices[i][0] + min(sumPrices[i - 1][1], sumPrices[i - 1][2])
-    sumPrices[i][1] = sumPrices[i][1] + min(sumPrices[i - 1][0], sumPrices[i - 1][2])
-    sumPrices[i][2] = sumPrices[i][2] + min(sumPrices[i - 1][0], sumPrices[i - 1][1])
+    sum0 = min(sumPrices[i - 1][1], sumPrices[i - 1][2]) + prices[i][0]
+    sum1 = min(sumPrices[i - 1][0], sumPrices[i - 1][2]) + prices[i][1]
+    sum2 = min(sumPrices[i - 1][0], sumPrices[i - 1][1]) + prices[i][2]
+    sumPrices.append((sum0, sum1, sum2))
 
-print(min(sumPrices[N - 1][0], sumPrices[N - 1][1], sumPrices[N - 1][2]))
+print(min(sumPrices[N - 1]))
