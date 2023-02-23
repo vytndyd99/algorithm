55513965c177411ebe3ffb0a317eb5ff6cf00c0c
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-23 14:29:01 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 576 ms, Memory: 59416 KB -BaekjoonHub

--- 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -1,37 +1,41 @@
 import sys
 import collections
-
-sys.setrecursionlimit(10 ** 6) # 재귀 횟수 제한 증가
+sys.setrecursionlimit(10 ** 8)
 
 N, M, R = map(int, sys.stdin.readline().split(" "))
 
 graph = [[] for i in range(N + 1)]
 
-ans = [0 for i in range(N + 1)]
+visited = [False for i in range(N + 1)]
 
-seq = 1
+ans = [0 for i in range(N + 1)]
 
 for i in range(M):
     a, b = map(int, sys.stdin.readline().split(" "))
     graph[a].append(b)
     graph[b].append(a)
 
+for s in graph:
+    s.sort(reverse=True)
+
+seq = 0
+
 def BFS(start):
     global seq
-
-    q = collections.deque([start]) # 처음 좌표
+    seq += 1
     ans[start] = seq
-    
+    visited[start] = True
+    q = collections.deque([start])
     while q:
-        start = q.popleft()
-        graph[start].sort(reverse=True)
-        for s in graph[start]:
-            if(ans[s] == 0):
+        dot = q.popleft()
+        for s in graph[dot]:
+            if(visited[s] == False):
+                visited[s] = True
                 seq += 1
                 ans[s] = seq
                 q.append(s)
 
 BFS(R)
 
-for i in range(1, len(ans)):
+for i in range(1, N + 1):
     print(ans[i])
