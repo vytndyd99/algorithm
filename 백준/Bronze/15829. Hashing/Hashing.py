32823835a86e626c169608d08b6d39ba166f42b4
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-24 14:30:08 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+L = int(sys.stdin.readline())
+
+s = sys.stdin.readline().rstrip()
+
+sumNum = 0
+
+for i in range(len(s)):
+    sumNum += (ord(s[i]) - 96) * int(math.pow(31, i))
+
+print(sumNum)
