b72880b7daa19a98206101e3163cfe735471ceda
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-02-24 18:51:13 +0900

    [unrated] Title: 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151137. 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
+++ 프로그래머스/unrated/151137. 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기/자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기.sql
@@ -1,5 +1,5 @@
-select CAR_TYPE, count(CAR_TYPE) as "CARS"
+select CAR_TYPE, count(CAR_ID) as CARS
 from CAR_RENTAL_COMPANY_CAR
-where OPTIONS like "%시트%"
+where OPTIONS like "%통풍시트%" or OPTIONS like "%가죽시트%" or OPTIONS like "%열선시트%"
 group by CAR_TYPE
 order by CAR_TYPE
