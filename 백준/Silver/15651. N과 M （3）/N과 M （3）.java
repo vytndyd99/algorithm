0a90302cca5f3d11a91ff2b963703762ad5eb1d2
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-03-17 17:49:10 +0900

    [Silver III] Title: N과 M (3), Time: 892 ms, Memory: 167864 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,52 @@
+// created by Ipad
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static boolean[] visited;
+    private static int N, M;
+    private static Stack<Integer> answer = new Stack<>();
+
+    private static void backTracking(int deep) throws IOException{
+        if(deep == M) {
+            int count = answer.size();
+            for(int i = 0 ; i < count ; i++) {
+                if(i == count - 1) {
+                    bw.write(Integer.toString(answer.elementAt(i)));
+                }
+                else {
+                    bw.write(Integer.toString(answer.elementAt(i)));
+                    bw.write(" ");
+                }
+            }
+            bw.write("\n");
+            return;
+        }
+        else {
+            for(int i = 1; i <= N ; i++) {
+                answer.push(i);
+                backTracking(deep + 1);
+                answer.pop();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        
+
+        // 처음 깊이를 0으로 지정
+        backTracking(0);
+
+        bw.flush();
+        
+        br.close();
+        bw.close();
+    }
+}
