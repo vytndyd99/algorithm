9556fae232ec18d78d8936e5cbfb3f1d46128a9c
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-03-28 22:21:39 +0900

    [Silver II] Title: 최대 힙, Time: 336 ms, Memory: 26256 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st; 
+    private static PriorityQueue<Integer> minqueue = new PriorityQueue<>(Collections.reverseOrder());
+
+
+
+    public static void main(String[] args) throws IOException{
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < N ; i++) {
+            int info = Integer.parseInt(br.readLine());
+            if(info == 0) {
+                if(minqueue.size() == 0) {
+                    bw.write("0");
+                    bw.write("\n");
+                }
+                else {
+                    bw.write(Integer.toString(minqueue.poll()));
+                    bw.write("\n");
+                }
+            }
+            else {
+                minqueue.add(info);
+            }
+        }
+
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
