22211d681d2e9000fe1d43dad9cd57966cb11407
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-06-03 19:35:25 +0900

    [level 1] Title: 모의고사, Time: 1.12 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,34 +1,70 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] answers) {
-        List<Integer> answer = new ArrayList<>();
+    public LinkedList solution(int[] answers) {
+        LinkedList<Integer> answer = new LinkedList<>();
         
-        int[] answer1 = {1,2,3,4,5};
-        int[] answer2 = {2,1,2,3,2,4,2,5};
-        int[] answer3 = {3,3,1,1,2,2,4,4,5,5};
-        int[] oldAnswer = {0, 0, 0};
+        int[] fir = {1,2,3,4,5}; // 1번 수포자
+        int[] sec = {2,1,2,3,2,4,2,5}; // 2번 수포자
+        int[] thd = {3,3,1,1,2,2,4,4,5,5}; // 3번 수포자
+        
+        int firAns = 0;
+        int secAns = 0;
+        int thdAns = 0;
         
         for(int i = 0 ; i < answers.length ; i++) {
-            int answerNum = answers[i];
-            if(answer1[i % 5] == answerNum) {
-                oldAnswer[0] += 1;
-            }
-            if(answer2[i % 8] == answerNum) {
-                oldAnswer[1] += 1;
-            }
-            if(answer3[i % 10] == answerNum) {
-                oldAnswer[2] += 1;
+            for(int j = 1 ; j <= 3 ; j++) {
+                if(j == 1) {
+                    if(answers[i] == fir[i % 5]) {
+                        firAns += 1;
+                    }
+                }
+                else if(j == 2) {
+                    if(answers[i] == sec[i % 8]) {
+                        secAns += 1;
+                    }
+                }
+                else if(j == 3) {
+                    if(answers[i] == thd[i % 10]) {
+                        thdAns += 1;
+                    }
+                }
             }
         }
         
-        int maxNum = Arrays.stream(oldAnswer).max().getAsInt();
+        int maxSolve = 0;
         
-        for(int i = 0 ; i < oldAnswer.length ; i++) {
-            if(oldAnswer[i] == maxNum) {
-                answer.add(i + 1);
+        if(maxSolve < firAns) {
+            maxSolve = firAns;
+        }
+        
+        if(maxSolve < secAns) {
+            maxSolve = secAns;
+        }
+        
+        if(maxSolve < thdAns) {
+            maxSolve = thdAns;
+        }
+        
+        for(int i = 1 ; i <= 3 ; i++) {
+            if(i == 1) {
+                if(maxSolve == firAns) {
+                    answer.add(i);
+                }
+            }
+            else if(i == 2) {
+                if(maxSolve == secAns) {
+                    answer.add(i);
+                }
+            }
+            else if(i == 3) {
+                if(maxSolve == thdAns) {
+                    answer.add(i);
+                }
             }
         }
+        
+        
         
         return answer;
     }
