38b24cf4e578da1771bb3a552dcce1b5a6f987f3
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-06-17 23:28:38 +0900

    [D1] Title: 간단한 N 의 약수, Time: 107 ms, Memory: 18,224 KB -BaekjoonHub

---
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringTokenizer st;
+
+    private static void solution(int N) throws IOException{
+        HashSet<Integer> list = new HashSet<>();
+
+        for(int i = 1 ; i <= (int)Math.sqrt(N) ; i++) {
+            if(N % i == 0) {
+                list.add(i);
+                list.add(N / i);
+            }
+        }
+
+        LinkedList<Integer> sortList = new LinkedList<>(list);
+        Collections.sort(sortList);
+
+        int size = sortList.size();
+
+        for(int i = 0 ; i < size ; i++) {
+            bw.write(Integer.toString(sortList.poll()));
+            if(i != size - 1) {
+                bw.write(" ");
+            }
+        }
+
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        
+        int N = Integer.parseInt(br.readLine());
+
+        solution(N);
+
+
+
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
