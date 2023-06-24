e636c075f5850d3dbb89b2be3c2333787877ffd9
vytndyd99 <41277173+vytndyd99@users.noreply.github.com>
2023-06-25 01:23:35 +0900

    SWEA 23.06.25
    문제풀이 진행

--- SW Expert Academy/D2/1983. 조교의 성적 매기기/Solution.java
+++ SW Expert Academy/D2/1983. 조교의 성적 매기기/Solution.java
@@ -19,16 +19,71 @@ public class Solution {
             st = new StringTokenizer(br.readLine(), " ");
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
+            double[] sums = new double[N];
+            String score = "";
+
+            double findSum = 0;
 
             for(int j = 0 ; j < N ; j++) {
                 st = new StringTokenizer(br.readLine(), " ");
-                int midterm = Integer.parseInt(st.nextToken()) * 0.35;
-                int finalterm = Integer.parseInt(st.nextToken()) * 0.45;
-                int assignment = Integer.parseInt(st.nextToken()) * 0.2;
-                int sumScore = midterm + finalterm + assignment;
+                double midterm = Integer.parseInt(st.nextToken()) * 0.35;
+                double finalterm = Integer.parseInt(st.nextToken()) * 0.45;
+                double assignment = Integer.parseInt(st.nextToken()) * 0.2;
+                double sumScore = midterm + finalterm + assignment;
+                sums[j] = sumScore;
 
-                
+                if(j == K - 1) {
+                    findSum = sumScore;
+                }
             }
+
+            Arrays.sort(sums);
+
+            int findIdx = 0;
+
+            for(int j = 0 ; j < sums.length ; j++) {
+                if(sums[j] == findSum) {
+                    findIdx = j;
+                    break;
+                }
+            }
+
+            if(findIdx < N / 10 * 1) {
+                score = "D0";
+            }
+            else if(findIdx < N / 10 * 2) {
+                score = "C-";
+            }
+            else if(findIdx < N / 10 * 3) {
+                score = "C0";
+            }
+            else if(findIdx < N / 10 * 4) {
+                score = "C+";
+            }
+            else if(findIdx < N / 10 * 5) {
+                score = "B-";
+            }
+            else if(findIdx < N / 10 * 6) {
+                score = "B0";
+            }
+            else if(findIdx < N / 10 * 7) {
+                score = "B+";
+            }
+            else if(findIdx < N / 10 * 8) {
+                score = "A-";
+            }
+            else if(findIdx < N / 10 * 9) {
+                score = "A0";
+            }
+            else if(findIdx < N / 10 * 10) {
+                score = "A+";
+            }
+
+            bw.write("#");
+            bw.write(Integer.toString(i + 1) + " ");
+            bw.write(score);
+            bw.write("\n");
+
 
         }
 
