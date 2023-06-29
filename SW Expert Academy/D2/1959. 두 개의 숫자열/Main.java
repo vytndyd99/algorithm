20a078fb34747ebb0c674a0f0e6ed3bd2871d045
vytndyd99 <41277173+vytndyd99@users.noreply.github.com>
2023-06-29 20:11:27 +0900

    23.06.29 SWEA 문제풀이
    SWEA 문제풀이

---
+++ SW Expert Academy/D2/1959. 두 개의 숫자열/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        arr.add(10);
+
+        System.out.println(arr.size());
+
+        
+    }
+    
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    Person() {
+        
+    }
+
+}
